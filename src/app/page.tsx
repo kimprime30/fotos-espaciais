9cfb4b3c6e73a9d7f297b4a94f2048dce92e7158
Francisco Aquino <106496532+kimprime30@users.noreply.github.com>
2025-06-30 02:30:34 +0000

    configuração dos states

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,29 @@
 
 import { photoList } from "@/data/photoList";
 import { PhotoItem } from "./components/PhotoItem";
+import { useState } from "react";
 
 export default function Home() {
+  const [selectedPhoto, setSelectedPhoto] = useState('')
+  const [showModal, setShowModal] = useState(false)
+
+const opemModal = (id:number) => {
+  const photoSelected = photoList.find((item ) =>item.id === id)
+  if(photoSelected) {
+    setSelectedPhoto(photoSelected.url)
+    setShowModal(true)
+  }
+}
+
   return (
     <div>
       <h1>Fotos Espaciais</h1>
-      <section>
+      <section >
         {photoList.map((item) => (
           <PhotoItem
           key={item.id}
           photo={item}
-          onClick = {() => {}}
+          onClick = {() => {opemModal(item.id)}}
           />
         ))}
       </section>
